9529063a1cddfb50c7ce21cc1e7af34160129453
Adam Baxter <abaxter@redhat.com>
2013-10-25 17:12:44 -0400

    Added Equals and Hash functions

--- src/main/java/org/jbpm/ee/support/AwareStatefulKnowledgeSession.java
+++ src/main/java/org/jbpm/ee/support/AwareStatefulKnowledgeSession.java
@@ -54,6 +54,32 @@ public class AwareStatefulKnowledgeSession implements KieSession {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((delegate == null) ? 0 : delegate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AwareStatefulKnowledgeSession other = (AwareStatefulKnowledgeSession) obj;
+		if (delegate == null) {
+			if (other.delegate != null)
+				return false;
+		} else if (!delegate.equals(other.delegate))
+			return false;
+		return true;
+	}
+
+	@Override
 	public void destroy() {
 		 delegate.destroy();
 	}
