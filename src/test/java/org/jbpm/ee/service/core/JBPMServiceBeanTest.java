a8d00c139d0c07d02df88e450af4cfb83058a1ea
Adam Baxter <abaxter@redhat.com>
2013-10-25 17:14:37 -0400

    If RuntimeService is CDI injected, everything works ok. EJB, it's not.

--- src/test/java/org/jbpm/ee/service/core/JBPMServiceBeanTest.java
+++ src/test/java/org/jbpm/ee/service/core/JBPMServiceBeanTest.java
@@ -1,15 +1,23 @@
 package org.jbpm.ee.service.core;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ejb.EJB;
+import javax.inject.Inject;
 
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
 import org.jboss.arquillian.transaction.api.annotation.Transactional;
-import org.jbpm.ee.cdi.JBPMServiceBean;
+import org.jbpm.ee.service.ProcessRuntimeBean;
+import org.jbpm.ee.service.RuntimeServiceBean;
 import org.jbpm.ee.service.TaskServiceBean;
+import org.jbpm.ee.service.remote.RuntimeServiceRemote;
 import org.jbpm.ee.startup.KnowledgeManagerBean;
 import org.jbpm.ee.support.KieReleaseId;
 import org.junit.Assert;
@@ -17,6 +25,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.task.model.TaskSummary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,14 +34,14 @@ public class JBPMServiceBeanTest extends BaseJBPMServiceTest {
 
 	private static final Logger LOG = LoggerFactory.getLogger(JBPMServiceBeanTest.class);
 	
-	@EJB
-	JBPMServiceBean jbpmServiceBean;
-	
-	@EJB
-	KnowledgeManagerBean knowledgeAgentManagerBean;
+	@Inject
+	RuntimeServiceBean runtimeServiceBean;
 	
 	@EJB
 	KnowledgeManagerBean knowledgeManagerBean;
+	
+	@EJB
+	ProcessRuntimeBean processRuntimeBean;
 	
 	@EJB
 	TaskServiceBean taskServicesBean;
@@ -42,14 +51,14 @@ public class JBPMServiceBeanTest extends BaseJBPMServiceTest {
 	@Test
 	@Transactional(value=TransactionMode.DEFAULT)
 	public void testJBPMServiceLookup() throws Exception {
-		Assert.assertTrue(jbpmServiceBean != null);
+		Assert.assertTrue(runtimeServiceBean != null);
 		
 		LOG.info("Hello world!");
+		runtimeServiceBean.setRuntime(kri);
+		KieSession sns1 = runtimeServiceBean.getKnowledgeSession();
+		KieSession sns2 = runtimeServiceBean.getKnowledgeSession();
 		
-		KieSession sns1 = knowledgeManagerBean.getKnowledgeSession(kri);
-		KieSession sns2 = knowledgeManagerBean.getKnowledgeSession(kri);
-		
-		Assert.assertTrue(sns1 != sns2);
+		Assert.assertEquals(sns1, sns2);
 		
 		LOG.info("IDs: "+sns1.getId()+" , "+sns2.getId());
 	}
@@ -57,17 +66,39 @@ public class JBPMServiceBeanTest extends BaseJBPMServiceTest {
 	@Test
 	@Transactional(value=TransactionMode.DEFAULT)
 	public void testSimpleProcess() throws Exception {
-		KieSession sns = knowledgeManagerBean.getKnowledgeSession(kri);
+		runtimeServiceBean.setRuntime(kri);
 		final String processString = "testProj.testProcess";
 		final String variableKey = "processString";
 		
 		Map<String, Object> processVariables = new HashMap<String, Object>();
 		processVariables.put(variableKey, "Initial");
 		
-		ProcessInstance processInstance = sns.startProcess(processString, processVariables);
-		Long processKey = processInstance.getId();
-		LOG.info("Process " + processKey + " started.");
+		ProcessInstance processInstance = processRuntimeBean.startProcess(processString, processVariables);
+		assertNotNull(processInstance);
+        assertEquals(ProcessInstance.STATE_ACTIVE, processInstance.getState());
 		
-		LOG.info("process: " + processKey + " - " + processInstance.getState());
+		List<TaskSummary> tasks = taskServicesBean.getTasksAssignedAsPotentialOwner("abaxter", "en-UK");
+		for(TaskSummary summary : tasks) {
+			LOG.info("Task: " + summary.getId());
+		}
+		assertNotNull(tasks);
+        assertEquals(1, tasks.size());
+        
+        long taskId = tasks.get(0).getId();
+        
+        Map<String,Object> testResults = new HashMap<String,Object>();
+        
+        taskServicesBean.claim(taskId, "abaxter");
+        taskServicesBean.start(taskId, "abaxter");
+        taskServicesBean.complete(taskId, "abaxter", testResults);
+     // check the state of process instance
+        
+        tasks = taskServicesBean.getTasksAssignedAsPotentialOwner("abaxter", "en-UK");
+		assertNotNull(tasks);
+        assertEquals(0, tasks.size());
+        
+        processInstance = processRuntimeBean.getProcessInstance(processInstance.getId());
+        
+        assertNull(processInstance);
 	}
 }
