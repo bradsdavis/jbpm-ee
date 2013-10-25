a2c73b65bf67da48b288168621f4b35ef074f422
Adam Baxter <abaxter@redhat.com>
2013-10-25 15:52:38 -0400

    Moved various items to RuntimeServiceBean. Cleaned up a few remaining pieces.

--- src/main/java/org/jbpm/ee/service/RuntimeServiceBean.java
+++ src/main/java/org/jbpm/ee/service/RuntimeServiceBean.java
@@ -4,9 +4,12 @@ import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
 
+import org.jbpm.ee.exception.SessionException;
 import org.jbpm.ee.service.remote.RuntimeServiceRemote;
 import org.jbpm.ee.startup.KnowledgeManagerBean;
+import org.jbpm.ee.support.AwareStatefulKnowledgeSession;
 import org.jbpm.ee.support.KieReleaseId;
+import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 
 @Stateful
@@ -41,6 +44,17 @@ public class RuntimeServiceBean  implements RuntimeServiceRemote{
 			return true;
 		}
 		return false;
+	}
+	
+	
+	/**
+	 * Sets up a new KnowledgeSession with the Human Task Handler defined.
+	 * 
+	 * @return
+	 * @throws SessionException
+	 */
+	public KieSession getKnowledgeSession() throws SessionException {
+		return new AwareStatefulKnowledgeSession(runtimeEngine.getKieSession());
 	}
 
 	
