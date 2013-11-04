c991ccb394add57ccd6992299cfa34de8b7e8589
Adam Baxter <abaxter@redhat.com>
2013-11-04 15:11:51 -0500

    Added EJB decorators

--- src/main/java/org/jbpm/ee/service/ExecutorServiceBean.java
+++ src/main/java/org/jbpm/ee/service/ExecutorServiceBean.java
@@ -1,11 +1,17 @@
 package org.jbpm.ee.service;
 
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 
 import org.jbpm.ee.service.remote.ExecutorServiceRemote;
 import org.kie.api.command.Command;
 import org.kie.api.runtime.CommandExecutor;
 
+@Stateful
+@LocalBean
+@ConversationScoped
 public class ExecutorServiceBean implements ExecutorServiceRemote {
 
 	@Inject
