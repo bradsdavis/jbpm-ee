bd6e53cb9b4b14ef988907a452e44f0a82e44058
Adam Baxter <abaxter@redhat.com>
2013-11-07 15:11:21 -0500

    Modified executor services

--- src/main/java/org/jbpm/ee/service/remote/ExecutorServiceRemote.java
+++ src/main/java/org/jbpm/ee/service/remote/ExecutorServiceRemote.java
@@ -2,9 +2,12 @@ package org.jbpm.ee.service.remote;
 
 import javax.ejb.Remote;
 
+import org.jbpm.ee.support.DeploymentAware;
+import org.kie.api.command.Command;
 import org.kie.api.runtime.CommandExecutor;
 
 @Remote
-public interface ExecutorServiceRemote extends CommandExecutor {
+public interface ExecutorServiceRemote extends CommandExecutor, DeploymentAware {
 
+	public <T> T executeTask(Command<T> command);
 }
