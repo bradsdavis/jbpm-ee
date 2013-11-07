242c9c73b6c5a981daaa2f7bc449f17b854fb4c7
Adam Baxter <abaxter@redhat.com>
2013-11-07 15:12:20 -0500

    Added JMS test. To be deleted!

---
+++ src/test/java/org/jbpm/ee/service/core/BaseJMSTest.java
@@ -0,0 +1,77 @@
+package org.jbpm.ee.service.core;
+
+import static org.jbpm.ee.test.util.KJarUtil.createKieJar;
+import static org.jbpm.ee.test.util.KJarUtil.getPom;
+import static org.junit.Assert.assertEquals;
+import static org.kie.scanner.MavenRepository.getMavenRepository;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.EJB;
+
+import org.drools.compiler.kie.builder.impl.InternalKieModule;
+import org.drools.core.command.runtime.process.StartProcessCommand;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.jbpm.ee.jms.BaseAsyncCommandExecutorBean;
+import org.jbpm.ee.support.KieReleaseId;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.KieServices;
+import org.kie.scanner.MavenRepository;
+
+@RunWith(Arquillian.class)
+public class BaseJMSTest extends BaseJBPMServiceTest {
+
+	private static final KieReleaseId kri = new KieReleaseId("com.redhat.demo", "testProj", "1.0-SNAPSHOT");
+	
+	@EJB
+	BaseAsyncCommandExecutorBean cmdExecutor;
+	
+	@Before
+    public void prepare() {
+		KieServices ks = KieServices.Factory.get();
+        List<String> processes = new ArrayList<String>();
+        processes.add("src/test/resources/kjar/testProcess.bpmn2");
+        InternalKieModule kjar = createKieJar(ks, kri.toReleaseIdImpl(), processes);
+        File pom = new File("target/kmodule", "pom.xml");
+        pom.getParentFile().mkdir();
+        try {
+            FileOutputStream fs = new FileOutputStream(pom);
+            fs.write(getPom(kri).getBytes());
+            fs.close();
+        } catch (Exception e) {
+            
+        }
+        MavenRepository repository = getMavenRepository();
+        repository.deployArtifact(kri.toReleaseIdImpl(), kjar, pom);
+    }
+	
+	@Test
+	@Transactional(value=TransactionMode.DEFAULT)
+	public void testSimpleProcess() throws Exception {
+		final String processString = "testProj.testProcess";
+		final String variableKey = "processString";
+		
+		Map<String, Object> processVariables = new HashMap<String, Object>();
+		processVariables.put(variableKey, "Initial");
+		
+		StartProcessCommand startProcess = new StartProcessCommand(processString, processVariables);
+		String correlationId = cmdExecutor.execute(kri, startProcess);
+		String correlationReturnId = null;
+		int count = 0;
+		while (correlationReturnId == null && count < 1) {
+			correlationReturnId = (String) cmdExecutor.pollResponse(correlationId);
+			count += 1;
+		}
+		
+		assertEquals(correlationId, correlationReturnId);
+	}
+}
