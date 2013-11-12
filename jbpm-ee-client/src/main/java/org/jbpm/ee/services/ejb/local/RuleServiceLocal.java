b8498151f21e86303180ae80519b27887ab43a6e
Adam Baxter <abaxter@redhat.com>
2013-11-12 15:53:19 -0500

    Added local interfaces.

---
+++ jbpm-ee-client/src/main/java/org/jbpm/ee/services/ejb/local/RuleServiceLocal.java
@@ -0,0 +1,16 @@
+package org.jbpm.ee.services.ejb.local;
+
+import javax.ejb.Local;
+
+import org.jbpm.ee.services.RuleService;
+
+
+/**
+ * 
+ * @author bdavis, abaxter
+ *
+ */
+@Local
+public interface RuleServiceLocal extends RuleService {
+
+}
---
+++ jbpm-ee-client/src/main/java/org/jbpm/ee/services/ejb/local/WorkItemServiceLocal.java
@@ -0,0 +1,15 @@
+package org.jbpm.ee.services.ejb.local;
+
+import javax.ejb.Local;
+
+import org.jbpm.ee.services.WorkItemService;
+
+/**
+ * 
+ * @author bdavis, abaxter
+ *
+ */
+@Local
+public interface WorkItemServiceLocal extends WorkItemService {
+
+}
